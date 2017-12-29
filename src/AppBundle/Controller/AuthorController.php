24de1073f27cc358c5a0eabd1adefce21152e3f8
Fedorov Ilya <fedyi95@mail.ru>
2017-12-29 21:19:23 +0300

    integrated with DB
    added editing of author info

--- src/AppBundle/Controller/AuthorController.php
+++ src/AppBundle/Controller/AuthorController.php
@@ -77,7 +77,59 @@ class AuthorController extends Controller{
      * @Route("/author/edit/{id}", name="author_edit")
      */
     public function editAction($id, Request $request){
-        return $this->render('author/edit.html.twig');
+        $author = $this->getDoctrine()->getRepository('AppBundle:Author')->find($id);
+
+        $author->setSurname($author->getSurname());
+        $author->setName($author->getName());
+        $author->setPatronymic($author->getPatronymic());
+
+        $form = $this->createFormBuilder($author)
+            ->add('surname', TextType::class, array(
+                'label' => 'Фамилия',
+                'attr' => array(
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px')))
+            ->add('name', TextType::class, array(
+                'label' => 'Имя',
+                'attr' => array(
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px')))
+            ->add('patronymic', TextType::class, array(
+                'label' => 'Отчество',
+                'required' => false,
+                'attr' => array(
+                    'class' => 'form-control',
+                    'style' => 'margin-bottom:15px')))
+            ->add('Save', SubmitType::class, array(
+                'label' => 'Внести изменения',
+                'attr' => array(
+                    'class' => 'btn btn-success',
+                    'style' => 'margin-bottom:15px')))
+            ->getForm();
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            //get data
+            $surname = $form['surname']->getData();
+            $name = $form['name']->getData();
+            $patronymic = $form['patronymic']->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $author = $em->getRepository('AppBundle:Author')->find($id);
+
+            $author->setSurname($surname);
+            $author->setName($name);
+            $author->setPatronymic($patronymic);
+
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Информация об авторе изменена'
+            );
+            return $this->redirectToRoute('author_list');
+        }
+
+        return $this->render('author/edit.html.twig', array('author' => $author, 'form' => $form->createView()));
     }
 
     /**
