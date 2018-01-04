21b88e94068d584e8ba411982d90a15a26184a28
Fedorov Ilya <fedyi95@mail.ru>
2018-01-04 21:14:13 +0300

    9) Generate entity Book

---
+++ src/fedy95/CatalogBundle/Entity/Book.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace fedy95\CatalogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity(repositoryClass="fedy95\CatalogBundle\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fileName", type="string", length=100)
+     */
+    private $fileName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="ISBN", type="string", length=30, nullable=true)
+     */
+    private $iSBN;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="pageNumber", type="integer", nullable=true)
+     */
+    private $pageNumber;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="yearPublication", type="integer", nullable=true)
+     */
+    private $yearPublication;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="imageName", type="string", length=100, nullable=true)
+     */
+    private $imageName;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set fileName
+     *
+     * @param string $fileName
+     *
+     * @return Book
+     */
+    public function setFileName($fileName)
+    {
+        $this->fileName = $fileName;
+
+        return $this;
+    }
+
+    /**
+     * Get fileName
+     *
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->fileName;
+    }
+
+    /**
+     * Set iSBN
+     *
+     * @param string $iSBN
+     *
+     * @return Book
+     */
+    public function setISBN($iSBN)
+    {
+        $this->iSBN = $iSBN;
+
+        return $this;
+    }
+
+    /**
+     * Get iSBN
+     *
+     * @return string
+     */
+    public function getISBN()
+    {
+        return $this->iSBN;
+    }
+
+    /**
+     * Set pageNumber
+     *
+     * @param integer $pageNumber
+     *
+     * @return Book
+     */
+    public function setPageNumber($pageNumber)
+    {
+        $this->pageNumber = $pageNumber;
+
+        return $this;
+    }
+
+    /**
+     * Get pageNumber
+     *
+     * @return int
+     */
+    public function getPageNumber()
+    {
+        return $this->pageNumber;
+    }
+
+    /**
+     * Set yearPublication
+     *
+     * @param integer $yearPublication
+     *
+     * @return Book
+     */
+    public function setYearPublication($yearPublication)
+    {
+        $this->yearPublication = $yearPublication;
+
+        return $this;
+    }
+
+    /**
+     * Get yearPublication
+     *
+     * @return int
+     */
+    public function getYearPublication()
+    {
+        return $this->yearPublication;
+    }
+
+    /**
+     * Set imageName
+     *
+     * @param string $imageName
+     *
+     * @return Book
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get imageName
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+}
+
